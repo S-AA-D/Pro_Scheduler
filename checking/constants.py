c8caa6a67cba8311eb5a2cd023f81c96b34df47b
S-AA-D <117470483+S-AA-D@users.noreply.github.com>
2024-02-14 18:30:02 +0300

    Update constants.py

--- checking/constants.py
+++ checking/constants.py
@@ -1,2 +1,2 @@
 BASE_URL = "https://proscheduler.prometric.com/home"
-DRIVER_PATH =r"C:/Users/sz0d/OneDrive/Desktop/programing/python/projects/web_scraping/selenium/selenium_drvers/chromedriver_win32/chromedriver.exe"
+DRIVER_PATH =r"DRIVER PATH"
